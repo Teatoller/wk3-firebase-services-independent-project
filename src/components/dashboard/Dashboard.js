0dcd1cf6554103b1afbb06a861b0a52713f300b9
stevenennis <seotieno@safaricom.co.ke>
2025-02-02 16:55:20 +0300

    ft(projects): add projects component
    - add projects component
    - render projects component on dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "../helpers";
+import Projects from "../projects/Projects";
 
 export default function Dashboard({ handleLogout }) {
   return (
     <div>
-        <h2>User Dashboard</h2>
+      <h2>User Dashboard</h2>
       <Button onClick={handleLogout}>Logout</Button>
-      
+      <Projects />
     </div>
   );
 }
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>housing project</li>
+        <li>Tree planting exercise</li>
+        <li>Water tower project</li>
+        <li>Landscaping project</li>
+      </ul>
+    </div>
+  );
+}
