4ecbced20f6412331fc82bc143b1e9bc2238c86e
stevenennis <seotieno@safaricom.co.ke>
2025-02-07 09:38:59 +0300

    ft(skills component): add skills component
    - render static skills data

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function Skills() {
+  const skills = [
+    {
+      id: "1",
+      name: "JavaScript",
+    },
+    {
+      id: "2",
+      name: "HTML5",
+    },
+    {
+      id: "3",
+      name: "CSS3",
+    },
+  ];
   return (
-    <div>Skills component</div>
-  )
+    <div>
+      <h2>Skills component</h2>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill.id}>{skill.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
